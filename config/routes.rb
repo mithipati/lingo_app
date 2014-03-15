8bacf766acd6bef9d79edd73b912bf327ff04956
Neil Mithipati <neil.mithipati@gmail.com>
2014-03-14 20:21:57 -0500

    Failing test for StaticPages actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 LingoApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/contact"
+  root 'static_pages#home'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,28 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home page" do
 
-    it "should have the content 'Join a Group'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Join a Group')
-    end
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_title(full_title(page_title)) }
   end
+
+  describe "Home page" do
+    before { visit root_path }
+    let(:page_title) { ' ' }
+
+    it_should_behave_like "all static pages"
+    it { should have_content('Join a Group') }
+    it { should_not have_title('| Home') }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like "all static pages"
+    it { should have_content('twitter') }
+  end
+
 end
