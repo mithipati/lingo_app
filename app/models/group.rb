0fe45cd068d0a2be623ad358f0bbcb8c40af1889
Neil Mithipati <neil.mithipati@gmail.com>
2014-03-15 14:46:52 -0500

    Passing test for validations of Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 25 },
+            uniqueness: { case_sensitive: false }
+  validates :description, length: { maximum: 140 }
 end
