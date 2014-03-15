e15be4d3fe59787b1e51c6d53adfec656381ec41
Neil Mithipati <neil.mithipati@gmail.com>
2014-03-14 19:27:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+LingoApp::Application.config.secret_key_base = '39a848dd1da553ff60b45ff71ebf528845c8f12ea39b37044c1b27cb32f8b5b473188f4b3bb116b0f4342b21f169cad24b2d589b55779f32873c02e1f5884de7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LingoApp::Application.config.session_store :cookie_store, key: '_lingo_app_session'
