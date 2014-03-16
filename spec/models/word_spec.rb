c876df001918539e201ca4e6e92ae6ecc5720881
Neil Mithipati <neil.mithipati@gmail.com>
2014-03-16 16:49:58 -0500

    Add validations to Word model

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -7,5 +7,36 @@ describe Word do
   subject { @word }
 
   it { should respond_to(:name) }
+  it { should respond_to(:definition) }
+  it { should be_valid }
+
+  describe "when word is too long" do
+    before { @word.name = "a" * 16 }
+    it { should_not be_valid }
+  end
+
+  describe "when word definition is too long" do
+    before { @word.definition = "a" * 141 }
+    it { should_not be_valid }
+  end
+
+  describe "when word is not present" do
+    before { @word.name = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe "when word definition is not present" do
+    before { @word.definition = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe "when word is incorrectly formatted" do
+    before do
+      @word.name = "invalid1"
+      @word.save
+    end
+    it { should_not be_valid }
+    specify { expect(@word.errors.full_messages).to eq(["Name for word only allows letters"]) }
+  end
 
 end
