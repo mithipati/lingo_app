8b3688f2ad0130c462fb9b61826d85fd2b202a61
Neil Mithipati <neil.mithipati@gmail.com>
2014-03-25 18:46:00 -0500

    Add meme-form on AJAX request

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,14 @@ LingoApp::Application.routes.draw do
     end
   end
   resources :users, except: [:index]
+
   get '/add_meme/groups/:id', to: "groups#add_meme", as: "add_meme"
   get '/memes', to: "words#show"
   post '/memes', to: "words#create"
   get '/memes/show_meme', to: "memes#show_meme"
+
+
+
   root 'static_pages#home'
   match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
