54b2a1033b2e3a3155b077966344bdbbfacf1433
Neil Mithipati <neil.mithipati@gmail.com>
2014-04-24 13:10:08 -0700

    Add default images for Groups and Users

--- app/uploaders/group_image_uploader.rb
+++ app/uploaders/group_image_uploader.rb
@@ -17,12 +17,13 @@ class GroupImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+  def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+    'https://s3-us-west-1.amazonaws.com/lingo-app/logo/lingo-logo.png'
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
@@ -32,23 +33,23 @@ class GroupImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process :resize_to_fit => [75, 75]
-  end
+  # version :thumb do
+  #   process :resize_to_fit => [75, 75]
+  # end
 
-  version :medium do
-    process :resize_to_fit => [200, 200]
-  end
+  # version :medium do
+  #   process :resize_to_fit => [200, 200]
+  # end
 
-  version :large do
-    process :resize_to_fit => [400, 400]
-  end
+  # version :large do
+  #   process :resize_to_fit => [400, 400]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
